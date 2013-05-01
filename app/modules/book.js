542cb0d723fa7ba7606214b2ef4cbca4a11ad111
Francis Tseng <ftzeng@gmail.com>
2013-05-01 16:24:40 -0400

    adding book example

---
+++ app/modules/book.js
@@ -0,0 +1,105 @@
+define([
+			 "app"
+],
+function(app) {
+	var Book = app.module();
+
+	Book.Model = Backbone.Model.extend({
+		defaults: function() {
+			return {
+				book: {}
+			};
+		}
+	});
+
+	Book.Collection = Backbone.Collection.extend({
+		model: Book.Model,
+
+		// Where to get the data
+		url: function() {
+			return "/books.json";
+		},
+
+		// How to handle the data
+		parse: function(obj) {
+			return obj.data;
+		},
+
+		initialize: function(models, options) {
+			if (options) {
+				//this.bar = options.bar;
+			}
+		}
+	});
+
+	Book.Views.Item = Backbone.View.extend({
+		template:"book/item",
+		tagName:"li",
+		serialize: function() {
+			return {
+				model: this.model,
+				author: this.author
+			};
+		},
+
+		// Bind some events
+		events: {
+			click: "myFunc"
+		},
+
+		myFunc: function(ev) {
+			// do stuff
+			// Make this the active model
+			app.active = this.model;
+
+			// Render
+			this.render();
+		},
+
+		// Do some stuff before render
+		beforeRender: function() {
+
+			// If this item has been activated...
+			if ( app.active === this.model ) {
+				this.$el.siblings().removeClass("active");
+				this.$el.addClass("active");
+			}
+		}
+	});
+
+	Book.Views.List = Backbone.View.extend({
+		template: "book/list",
+		className: "book-class",
+
+		serialize: function() {
+			return function() {
+				// stuff
+			};
+		},
+
+		beforeRender: function() {
+			// For each book in collection
+			/*
+			this.books.each(function(book) {
+
+				// Insert a Book item view with book to the ul
+				this.insertView("ul", new Book.Views.Item({
+					model: book
+				}));
+
+			});
+			*/
+		},
+
+		initialize: function() {
+			// Listen to some events
+			this.listenTo(this.options.books, {
+				// Render when one of the books reset.
+				"reset": this.render
+			});
+		}
+	});
+
+	// Return the module for AMD compliance
+	return Book;
+});
--- app/router.js
+++ app/router.js
@@ -1,19 +1,61 @@
 define([
   // Application.
-  "app"
+  "app",
+
+	// Modules.
+	"modules/book"
 ],
 
-function(app) {
+function(app, Book) {
 
   // Defining the application router, you can attach sub routers here.
   var Router = Backbone.Router.extend({
+		initialize: function() {
+			var collections = {
+				books: new Book.Collection()
+			}
+
+			// Ensure the router has references to the collections
+			_.extend(this, collections);
+
+			app.useLayout("main").setViews({
+				".books": new Book.Views.List(collections)
+			}).render();
+		},
+
     routes: {
-      "": "index"
+      "": "index",
+			"book/:name": "book"
     },
 
     index: function() {
+			// Reset the state and render
+			this.reset();
 
-    }
+			this.books.fetch();
+    },
+
+		book: function(name) {
+			// Reset the state and render
+			this.reset();
+
+			// Set the book
+			this.books.book = name;
+
+			// Fetch the data
+			this.books.fetch();
+		},
+
+		reset: function() {
+			// Reset collections to initial state
+			if ( this.books.length ) {
+				this.books.reset();
+			}
+
+			// Reset active model
+			app.active = false;
+		}
+
   });
 
   return Router;
