7e1f5f2d0be31a3d8edc7e307e08835a0c37d08e
Francis Tseng <ftzeng@gmail.com>
2013-05-05 02:35:58 -0400

    powerup upgrade

---
+++ powerup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+tput setaf 4
+echo "POWERING UP..."
+tput sgr0
+
+git submodule init
+git submodule update
+git rm --cached app/styles
+git rm .gitmodules
+rm -rf app/styles/.git
+git add app/styles
+rm -rf .git
+rm powerup.sh
+rm guide.md
+rm readme.md
+git init
+
+if [[ ! "$(type -P jam)" ]]; then
+	echo "Jam was not found on your system."
+	echo "Visit jamjs.org and set it up,"
+	echo "then run 'jam upgrade' in this directory."
+else
+	jam upgrade
+fi
+
+tput setaf 4
+echo "READY TO GO."
+tput sgr0
