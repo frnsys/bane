d22f80553d15b587af050c6ee576a2df8277e8e1
Francis Tseng <ftzeng@gmail.com>
2013-05-09 11:28:11 -0400

    adding grunt jade

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 
 	// Configuration
+	// =======================================
 	grunt.initConfig({
 
 		// Watch
@@ -19,22 +20,41 @@ module.exports = function(grunt) {
 		}
 
 		// Compile SASS/SCSS
-		// Since all other stylesheets are included in index.scss,
+		// Since all other stylesheets are @import-ed in index.scss,
 		// that's the only one we need to compile.
 		sass: {
 			files: {
 				'app/styles/index.css': 'app/styles/index.scss'	
+			}
+		},
+
+		// Compile Jade
+		// http://goo.gl/eoSwR
+		jade: {
+			compile: {
+				options: {
+					pretty: true
+				},
+				files: grunt.file.expandMapping(['**/*.jade'], 'dist/', {
+					cwd: 'app/templates',
+					rename: function(destBase, destPath) {
+						return destBase + destPath.replace(/\.jade$/, '.html');
+					}
+				}
 			}
 		}
 
 	});
 
 	// Define grunt tasks
+	// =======================================
 	grunt.registerTask('default', ['watch']);
 
 	// Load grunt packages
+	// =======================================
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-jade');
 
 };
