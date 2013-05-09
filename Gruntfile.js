fa69c2207b0c8b41b8efdb4110f52a460234e5ee
Francis Tseng <ftzeng@gmail.com>
2013-05-09 11:12:34 -0400

    adding grunt sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,16 +10,22 @@ module.exports = function(grunt) {
 		},
 
 		// Compile CoffeeScript
-		// We only need to compile the
-		// main script, since, in dev,
-		// the other coffeescripts will be
-		// compiled on the fly.
+		// We only need to compile the main script, since, in dev,
+		// the other coffeescripts will be compiled on the fly.
 		coffee: {
 			compile: {
 				'app/main.js': 'app/main.coffee'
 			}
 		}
 
+		// Compile SASS/SCSS
+		// Since all other stylesheets are included in index.scss,
+		// that's the only one we need to compile.
+		sass: {
+			files: {
+				'app/styles/index.css': 'app/styles/index.scss'	
+			}
+		}
 
 	});
 
@@ -29,5 +35,6 @@ module.exports = function(grunt) {
 	// Load grunt packages
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
 };
