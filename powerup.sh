1ee1931b4d825f7114ab0e050e1379e4c2a21449
Francis Tseng <ftzeng@gmail.com>
2013-05-09 14:52:44 -0400

    added npm install for powerup

--- powerup.sh
+++ powerup.sh
@@ -4,16 +4,23 @@ tput setaf 4
 echo -e "\nPOWERING UP...\n"
 tput sgr0
 
+# Submodule loading
 git submodule init
 git submodule update
 git rm --cached app/styles
 git rm .gitmodules
 rm -rf app/styles/.git
 git add app/styles
+
+# Destroying git history
 rm -rf .git
+
+# Cleanup
 rm powerup.sh
 rm guide.md
 rm readme.md
+
+# Init new git repo
 git init
 
 if [[ ! "$(type -P jam)" ]]; then
@@ -21,7 +28,17 @@ if [[ ! "$(type -P jam)" ]]; then
 	echo "Visit jamjs.org and set it up,"
 	echo "then run 'jam upgrade' in this directory."
 else
+	echo "Upgrading Jam packages..."
 	jam upgrade
+fi
+
+if [[ ! "$(type -P npm)" ]]; then
+	echo "npm was not found on your system."
+	echo "Visit npmjs.org and set it up,"
+	echo "then run 'npm install' in this directory."
+else
+	echo "Installing npm dependencies..."
+	npm install
 fi
 
 tput setaf 4
