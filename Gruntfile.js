d965e80712b406d6910b1aac0e6e7b895dcc618e
Francis Tseng <ftzeng@gmail.com>
2013-05-10 14:24:53 -0400

    added grunt connect server

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,18 @@ module.exports = function(grunt) {
 	// Configuration
 	// =======================================
 	grunt.initConfig({
+		// Server
+		connect: {
+			server: {
+				options: {
+					port: 8989
+				}
+			}
+		},
 
 		// Watch
 		watch: {
+			options: { livereload: true },
 			files: ['vendor/**/*', 'dev/**/*', 'data/**/*'],
 			tasks: ['sass', 'jade']
 		},
@@ -34,22 +43,19 @@ module.exports = function(grunt) {
 					'app/templates.js': ['app/templates/**/*.jade']
 				}
 			}
-		},
-
-		// Clean out app/
-		clean: ['app/']
+		}
 
 	});
 
 	// Define grunt tasks
 	// =======================================
-	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('default', ['connect', 'watch']);
 
 	// Load grunt packages
 	// =======================================
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 
 };
